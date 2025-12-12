723b621f77d498e4263cae936fc2ce0c9aa3f25c
Gang Ferdinand Dinga <ferdinand.gang4@gmail.com>
2025-12-11 22:03:31 -0600

    Start up project setup with all dependencies

---
+++ BackEnd_Spring/backendmiufos/src/main/java/com/ferdi/backendmiufos/BackendmiufosApplication.java
@@ -0,0 +1,13 @@
+package com.ferdi.backendmiufos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendmiufosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendmiufosApplication.class, args);
+	}
+
+}
---
+++ BackEnd_Spring/backendmiufos/src/test/java/com/ferdi/backendmiufos/BackendmiufosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferdi.backendmiufos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendmiufosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
