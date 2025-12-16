395845993b95eff7b8d06a8077f2ffab1f79e969
Gang Ferdinand Dinga <ferdinand.gang4@gmail.com>
2025-12-16 17:23:17 -0600

    Integration of order view with the front end

--- frontendv2/src/services/Order_service.jsx
+++ frontendv2/src/services/Order_service.jsx
@@ -1,9 +1,9 @@
 
 import axios from "axios";
 
-const Order_API  = import.meta.env.Order_API_URL
+const Order_API = import.meta.env.VITE_Order_API_URL;
 
 export const fetchOrders = () => axios.get(Order_API);
-export const createOrder = (order) => axios.post(Order_API_URL, Order_API);
+export const createOrder = (order) => axios.post(Order_API, order);;
 export const updateOrder = (id, order) => axios.put(`${Order_API}/${id}`, order);
 export const deleteOrder = (id) => axios.delete(`${Order_API}/${id}`);
